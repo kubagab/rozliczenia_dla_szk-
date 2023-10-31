f162a04f72a0bbf36a032d103b0a5f77849edcda
Jakub Gabrylewicz <kubagab@icloud.com>
2023-11-01 00:40:36 +0100

    add API for registration

--- users/views.py
+++ users/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.views import View
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 # Create your views here.
-class Register(View):
+class RegisterAPI(APIView):
     def post(self, request):
+
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
