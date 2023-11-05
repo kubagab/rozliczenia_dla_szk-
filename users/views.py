da1c0fc5c1e71ab49de07e787f8b351edd5a7547
Jakub Gabrylewicz <kubagab@icloud.com>
2023-11-05 23:46:28 +0100

    added jwt

--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-import bcrypt as bcrypt
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 from rest_framework.views import APIView
@@ -10,26 +9,25 @@ from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
-import bycrypt
 
 
 # Create your views here.
 class RegisterAPI(APIView):
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        first_name = request.data.get('first_name')
-        last_name = request.data.get('last_name')
-        email = request.data.get('email')
-
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
+            user = User.objects.create_user(
+                username=request.data['username'],
+                password=request.data['password'],
+                email=request.data['email'],
+                first_name=request.data['first_name'],
+                last_name=request.data['last_name']
+            )
             if user:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginAPI(APIView):
